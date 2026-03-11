c57ea28f1abc4bd25ececf154444253519e72080
mcje <mathias.claus.jensen@gmail.com>
2026-03-11 18:24:30 +0100

    Remove outdated tests
    Most tests referenced the old pending_* system (mark_delete, mark_move,
    mark_create, etc.) which no longer exists. The architecture changed to
    diff-at-save-time, making these tests invalid.

--- tests/sap/minimal_init.lua
+++
@@ -1,7 +0,0 @@
--- Minimal init for running tests
--- Usage: nvim --headless -u tests/sap/minimal_init.lua -c "PlenaryBustedDirectory tests/sap"
-
-vim.opt.runtimepath:append(".")
-vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/lazy/plenary.nvim")
-
-vim.cmd.runtime("plugin/plenary.vim")
